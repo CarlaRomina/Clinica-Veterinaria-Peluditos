57f4b37b8a9e0deb098edd2edd76f917d78f2181
CRB <cr_baez@hotmail.com>
2022-10-23 17:45:38 -0300

    Subida paginas responsive

--- app.js
+++ app.js
@@ -47,3 +47,27 @@ function validar() {
     
 
 }
+
+
+// API
+
+
+let endpoint = 'https://api.binance.com/api/v3/ticker/price'
+fetch(endpoint)
+    .then( respuesta => respuesta.json() )
+    .then( datos => mostrarData(datos))
+    .catch( e => console.log(e))
+
+
+const mostrarData = (data)=>{
+    //console.log(data)
+    let body = ''
+    for (let i=0; i < data.length; i++) {
+        body = `<tr><td>${data[i].symbol}</td><td>${data[i].price}</td></tr>`
+        if(`<tr><td>${data[i].symbol}</td></tr>`==`<tr><td>BTCUSDT</td></tr>`)
+        {
+        document.getElementById('data').innerHTML = body
+        }
+    }
+    
+}    
